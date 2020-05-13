bc9ec47a408166ec3beb89c7b1e0f36e0c686581
Lech Różański <l.rozanski88@gmail.com>
2020-05-13 16:51:07 +0200

    Ignored test

--- src/test/kotlin/com/lrozanski/generator/dungeon/MapGeneratorTest.kt
+++ src/test/kotlin/com/lrozanski/generator/dungeon/MapGeneratorTest.kt
@@ -1,11 +1,13 @@
 package com.lrozanski.generator.dungeon
 
 import com.lrozanski.generator.dungeon.data.Size
+import jdk.nashorn.internal.ir.annotations.Ignore
 import org.junit.jupiter.api.Test
 
 internal class MapGeneratorTest {
 
     @Test
+    @Ignore
     fun generateMap() {
         MapGenerator(Size(64, 64)).generate()
     }
