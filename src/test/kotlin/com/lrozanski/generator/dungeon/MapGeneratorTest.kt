b44b8af4687756c64c46925c93e033bea3b04183
Lech Różański <l.rozanski88@gmail.com>
2020-05-13 16:55:53 +0200

    Switched to the correct annotation for JUnit 5

--- src/test/kotlin/com/lrozanski/generator/dungeon/MapGeneratorTest.kt
+++ src/test/kotlin/com/lrozanski/generator/dungeon/MapGeneratorTest.kt
@@ -1,13 +1,13 @@
 package com.lrozanski.generator.dungeon
 
 import com.lrozanski.generator.dungeon.data.Size
-import jdk.nashorn.internal.ir.annotations.Ignore
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class MapGeneratorTest {
 
     @Test
-    @Ignore
+    @Disabled
     fun generateMap() {
         MapGenerator(Size(64, 64)).generate()
     }
