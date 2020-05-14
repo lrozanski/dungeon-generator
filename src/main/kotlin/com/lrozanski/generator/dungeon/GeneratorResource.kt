5e96a91be644cddd6e11442a1c666af3d8e4de81
Lech Różański <l.rozanski88@gmail.com>
2020-05-14 19:46:23 +0200

    Added CrossOrigin to the rest service

--- src/main/kotlin/com/lrozanski/generator/dungeon/GeneratorResource.kt
+++ src/main/kotlin/com/lrozanski/generator/dungeon/GeneratorResource.kt
@@ -3,9 +3,11 @@ package com.lrozanski.generator.dungeon
 import com.lrozanski.generator.dungeon.data.Cell
 import com.lrozanski.generator.dungeon.data.Size
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = ["*"])
 @RestController("/")
 class GeneratorResource {
 
