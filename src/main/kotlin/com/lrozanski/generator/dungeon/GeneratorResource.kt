d35d72a4331a4e24b95318513e1198c772d8f413
Lech Różański <l.rozanski88@gmail.com>
2020-05-14 20:03:22 +0200

    Updated REST controller mapping

--- src/main/kotlin/com/lrozanski/generator/dungeon/GeneratorResource.kt
+++ src/main/kotlin/com/lrozanski/generator/dungeon/GeneratorResource.kt
@@ -3,15 +3,15 @@ package com.lrozanski.generator.dungeon
 import com.lrozanski.generator.dungeon.data.Cell
 import com.lrozanski.generator.dungeon.data.Size
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin
-@RestController("/generator")
+@RestController
+@RequestMapping("/generator", produces = [MediaType.APPLICATION_JSON_VALUE])
 class GeneratorResource {
 
-    @PostMapping("/generate", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping("/generate")
     fun generate(): List<Cell> {
         val mapGenerator = MapGenerator(Size(64, 64), 13..25)
         mapGenerator.generate()
